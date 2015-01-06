036f0bde533b6adf320a3d70537996b4ed253530
Craig Blaszczyk <masterjakul@gmail.com>
2015-01-06 20:50:51 +0000

    add documentation; add summarise_forecast function

--- forecaster/forecast.py
+++ forecaster/forecast.py
@@ -1,21 +1,30 @@
 # coding: utf-8
+from collections import defaultdict
 import requests
+import datetime
 from forecaster import ForecasterException
 
-def get_forecast(location, num_days=None, mode=None, units=None):
+def get_forecast(location, num_days=None, units=None):
+    """
+    Query the OpenWeatherMap API for the forecast for a given location.
+
+    :param location: Where to find the forecast of
+    :param num_days: How many days forecast to return, starting with today.
+    Optional. Defaults to 14.
+    :param units: `imperial` or `metric`. Optional. Defaults to 'imperial'
+    :return: The OWM API response content
+    :raises ForecasterException: If there was a problem getting the data from
+    the OWM API
+    """
     # TODO: cache the result for 10 minutes, as recommend by OWM
     if num_days is None:
         num_days = 14
-    if mode is None:
-        mode = 'json'
     if units is None:
         units = 'imperial'
 
-    #TODO: url escape place name? what about place names with non-ascii chars in them? e.g. Águeda
-
     url = (
-        u'http://api.openweathermap.org/data/2222.5/forecast/daily?q={location}'
-        u'&mode={mode}&units={units}&cnt={num_days}'.format(**locals())
+        u'http://api.openweathermap.org/data/2.5/forecast/daily?q={location}'
+        u'&mode=json&units={units}&cnt={num_days}'.format(**locals())
     )
 
     response = requests.get(url)
@@ -44,5 +53,32 @@ def get_forecast(location, num_days=None, mode=None, units=None):
 
     return data
 
-if __name__ == '__main__':
-    get_forecast(u'Águeda')
+def summarise_forecast(location):
+    """
+
+    :param location:
+    :return:
+    """
+    forecast = get_forecast(location)
+    summary = {'city': location, 'forecasts': defaultdict(set)}
+
+    summary['min'] = forecast[u'list'][0][u'temp'][u'min']
+    summary['max'] = forecast[u'list'][0][u'temp'][u'max']
+    for weather in (forecast['list']):
+        summary['min'] = min(weather[u'temp'][u'min'], summary['min'])
+        summary['max'] = max(weather[u'temp'][u'max'], summary['max'])
+        date = datetime.datetime.fromtimestamp(
+            weather[u'dt']
+        ).strftime('%Y-%m-%d')
+        for weather_ in weather[u'weather']:
+            # Note: There is a unicode to string conversion here, which would
+            # cause problems if any of the weather types used non-ascii
+            # characters
+            summary['forecasts'][str(weather_[u'main'])].add(date)
+
+    # Convert the forecasts to the expected output format
+    summary['forecasts'] = dict(summary['forecasts'])
+    for weather_type, dates in summary['forecasts'].items():
+        summary['forecasts'][weather_type] = list(dates)
+
+    return summary
